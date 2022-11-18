d7eba9ba01bf77e1710d36aa9e109875bf36ea87
Nickolas Orellana <p23norellanagonzalez@fairfieldprep.org>
2022-11-18 11:35:35 -0500

    done

--- Lab_7-1.py
+++ Lab_7-1.py
@@ -1,6 +1,8 @@
 # author NDO 11/16/22
-
+# create a function called greeting 
 def greeting() :
-    return('Hello World!')
-'''Creates a statement that substitutes '''
+# add a docstring that explains how the function can only print "Hello World"
+    '''Creates a statement that substitutes the variable in greeting to the statement 'Hello World' and prints it '''  
+    return 'Hello World!'
+# add a statement that prints "Hello World"
 print(greeting())
---
+++ Lab_7-2.py
@@ -0,0 +1,12 @@
+# author NDO 11/18/22 
+# create a function called "find_sum"
+# add the parameters num1 and num2 
+def find_sum(num1, num2):
+# add a statement to the funciton that adds the arguments and stores them in a new variable "num_sum"
+    num_sum = num1 + num2 
+#finish the function with a statement that returns num_sum
+    return(num_sum)
+# create a statemetn that calls the function to find the sum of 111 and 222 and set it equal to "my_sum"
+# add another statement that prints the value of the variable "my_sum"
+my_sum = find_sum(111,222)
+print(my_sum)
